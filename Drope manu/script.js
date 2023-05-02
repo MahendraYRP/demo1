c433dea91640531f207cd1b3fb41fb11fc3bc7e6
MahendraYRP <mahendra@yrpitsolutions.com>
2023-05-02 14:20:32 +0530

    update

---
+++ Drope manu/script.js
@@ -0,0 +1,11 @@
+// const dropdowns = document.querySelectorAll('.dropdown');
+
+// dropdowns.forEach(dropdown => {
+//   dropdown.addEventListener('mouseenter', () => {
+//     dropdown.querySelector('.dropdown-menu').classList.add('active');
+//   });
+
+//   dropdown.addEventListener('mouseleave', () => {
+//     dropdown.querySelector('.dropdown-menu').classList.remove('active');
+//   });
+// });
